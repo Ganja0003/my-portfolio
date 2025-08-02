f5cde2afadb9f8953f1eeaf4aab344ac42fcd8bc
yuusuf <yuusuf98@outlook.com>
2025-08-02 09:09:30 +0200

    Add hamburger menu on smaller screens size when showing the navlinks

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,4 +1,13 @@
+"use client";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const togglevisibility = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <>
       <div className="navbarContainer">
@@ -6,10 +15,17 @@ export default function Navbar() {
           <h1 className="navbarTitle">Yuusuf Roble</h1>
         </div>
         <div className="navbarRight">
-          <ul>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contacts</li>
+          <GiHamburgerMenu className="menuIcon" onClick={togglevisibility} />
+          <ul className={`navLinks ${menuOpen ? "show" : "hide"}`}>
+            <li onClick={togglevisibility}>
+              <a href="#about">About</a>
+            </li>
+            <li onClick={togglevisibility}>
+              <a href="#projects">Projects</a>
+            </li>
+            <li onClick={togglevisibility}>
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
         </div>
       </div>
