573555b98535c3bef63cb482c3fefd40db791a3e
yuusuf <yuusuf98@outlook.com>
2025-08-16 17:06:30 +0200

    Change the projects description and link to their respected images

--- src/app/components/projects/projectList.jsx
+++ src/app/components/projects/projectList.jsx
@@ -8,31 +8,31 @@ export default function ProjectList() {
           img='/pizza.jpeg'
           projectTitle="Mealsharing"
           techStack="FULLSTACK"
-          projectDescription="a combination of technologies used to build and run an application, website, or other software project"
+          projectDescription="In progress. Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
           viewProjectLink="https://github.com/Ganja0003/meal-sharing"
           viewSourceLink="https://github.com/Ganja0003/meal-sharing"
         />
          <Projects
-          img='/pizza.jpeg'
+          img='\Quiz-app-project.png'
           projectTitle="Quiz App"
           techStack="FRONTEND"
-          projectDescription="a combination of technologies used to build and run an application, website, or other software project"
+          projectDescription="Individual project: Its a small quiz game where a user can answer multiple choice questions, see score, and switch player."
           viewProjectLink="https://quiz-app-project-2ui6.onrender.com/"
           viewSourceLink="https://github.com/Ganja0003/Quiz-App-Project"
         />
          <Projects
-          img='/pizza.jpeg'
+          img='\tantakuy-project.png'
           projectTitle="Tantakuy"
           techStack="FULLSTACK"
-          projectDescription="a combination of technologies used to build and run an application, website, or other software project"
+          projectDescription="Team project: Final HYF project. Website where a company can create a competion and a participant can join competition and vote."
           viewProjectLink="https://tantakuy-hyf.vercel.app/"
           viewSourceLink="https://github.com/Lupescua/tantakuy-hyf"
         />
          <Projects
-          img='/pizza.jpeg'
+          img='/weather-app-project.png'
           projectTitle="Weather App"
           techStack="FRONTEND"
-          projectDescription="a combination of technologies used to build and run an application, website, or other software project"
+          projectDescription="Individual project: One of my first project, I followed a tutorial. Its an app that shows the current weather of a city, like temperature, wind, humidity."
           viewProjectLink="https://weather-app-6pug.onrender.com/"
           viewSourceLink="https://github.com/Ganja0003/weather-app"
         />
