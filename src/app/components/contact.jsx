be78430c7487e65e43a85bd44634b6f6df44c88e
yuusuf <yuusuf98@outlook.com>
2025-08-17 13:21:07 +0200

    Add responsive static contact form.

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -1,7 +1,27 @@
 export default function Contact() {
   return (
     <>
-      <h1>Contact</h1>
+
+      
+      <form action="" className='contactContainer'>
+        <h2 className='contactTitle'>Contact Me</h2>
+        <div className='textInput'>
+          <label htmlFor="name">Name</label> <input type="text" id="name" />
+        </div>
+        <div className='textInput'>
+          
+          <label htmlFor="name">Email</label> <input type="text" id="name" />
+        </div>
+        <div className='textArea'>
+          
+          <label htmlFor="name">Message</label>
+          <textarea ></textarea>
+        </div>
+
+        <button className='contactButton'>Send</button>
+      </form>
+    
     </>
+      
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
       <Navbar/>
       <About/>
       <ProjectList/>
+      <Contact/>
     </div>
     
     </>
